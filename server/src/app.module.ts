d3701b1a142dc5e7557c7e01a39827acf2227deb
Lyosha Zverev <alyoshkazverev20@gmail.com>
2022-11-16 13:47:09 +0400

    feat: add graphql(with error)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,17 @@ import { AppService } from './app.service';
 import { Users } from './users/users.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
