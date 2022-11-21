5f644ea5bd170802617baf32f448c8f9b1e52c2a
Lyosha Zverev <alyoshkazverev20@gmail.com>
2022-11-21 21:41:51 +0400

    feat: handle connect and disconnect user

--- server/src/users/users.gateway.ts
+++ server/src/users/users.gateway.ts
@@ -1,18 +1,32 @@
+import { Logger } from '@nestjs/common';
 import {
-  MessageBody,
-  SubscribeMessage,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { UsersService } from './users.service';
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway(5000, { cors: '*' })
-export class UsersGateway {
+export class UsersGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
-  server;
+  server: Server;
+  private logger: Logger = new Logger('UsersGateway');
+  constructor(private userService: UsersService) {}
 
-  @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string): void {
-    console.log(message);
-    this.server.emit('message', message);
+  afterInit() {
+    this.logger.log('Init');
+  }
+
+  handleConnection(client: Socket) {
+    console.log('Connect: ', client);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Disconnect: ', client);
   }
 }
