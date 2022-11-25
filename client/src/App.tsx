f534606cc38592c8ba0e6a79b40a6f10e3646fbe
Lyosha Zverev <alyoshkazverev20@gmail.com>
2022-11-25 22:01:58 +0400

    refactor: change pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,10 @@
 import "./assets/styles/global.sass";
 import { Router } from "./router/Router";
-import { MainTemplate } from "./templates/MainTemplate";
 
 function App() {
   return (
     <div className="App">
-      <MainTemplate>
-        <main className="main">
-          <Router />
-        </main>
-      </MainTemplate>
+      <Router />
     </div>
   );
 }
--- client/src/pages/Chats/Chats.tsx
+++ client/src/pages/Chats/Chats.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { MainTemplate } from "../../templates/MainTemplate";
 
 export const Chats: React.FC = () => {
-  return <h2>Chats</h2>;
+  return (
+    <MainTemplate>
+      <main className="main">
+        <h2>Chats</h2>
+      </main>
+    </MainTemplate>
+  );
 };
