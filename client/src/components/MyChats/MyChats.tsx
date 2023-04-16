dce103308769d7cce08911b5cebeb9f38f8b5dd6
Lyosha Zverev <alyoshkazverev20@gmail.com>
2023-04-16 18:47:22 +0400

    refactor: change get chats logic and update server port

--- client/src/components/MyChats/MyChats.tsx
+++ client/src/components/MyChats/MyChats.tsx
@@ -47,13 +47,6 @@ export const MyChats: React.FC = () => {
     refetch: refetchChats,
   } = useQuery(GET_MY_CHATS, {
     fetchPolicy: "network-only",
-    onCompleted: ({ getMyChats }) => {
-      if (currentUser.id) {
-        let dataChats: IChat[] = addUserFromToChat(getMyChats, currentUser.id);
-
-        setChats(dataChats);
-      }
-    },
     onError: (error) => {
       if (error.message === "Unauthorized") {
         localStorage.removeItem("token");
@@ -103,6 +96,14 @@ export const MyChats: React.FC = () => {
 
     return () => document.removeEventListener("keyup", (e) => escapeHandler(e));
   }, []);
+
+  useEffect(() => {
+    if (currentUser.id) {
+      let dataChats: IChat[] = addUserFromToChat(chatsData, currentUser.id);
+
+      setChats(dataChats);
+    }
+  }, [currentUser.id]);
 
   useEffect(() => {
     const { initObserverMessages, removeObserverMessages } = useObserveMessages(
